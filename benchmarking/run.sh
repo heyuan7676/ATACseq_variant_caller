ec4c8cf2bf6e6368de9f9541c481ca6c8e8c7d9f
Yuan <yuanhe777tt@hotmail.com>
2020-10-02 11:05:25 -0400

    Use snakemake

---
+++ benchmarking/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH -p skylake
+
+source activate snakemake
+#INDIV="$1" snakemake -s Snakefile --cores 24
+
+ml picard
+ml samtools
+ml gatk
+ml bowtie2
+
+## Run one snakemake job
+#INDIV="$1" snakemake -s Snakefile --cores 24
+
+
+## Run multiple jobs in parallel
+snakemake --use-conda --jobs 20 \
+    --cluster "sbatch --ntasks=24 --time=20:00:00 --partition parallel" \
+    --rerun-incomplete \
+    --keep-going \
+    $@
+
+# Note: need to specify conda environment in rule
+
+
+## failed
+#~/miniconda3/envs/snakemake/bin/snakemake -s Snakefile \
+#          --profile /home-4/yhe23@jhu.edu/.config/snakemake/slurm \
+#          --rerun-incomplete \
+#          --use-conda
+# Problem: the submitted job can't find the snakemake module
+# Error: No module named snakemake.utils
+
+
