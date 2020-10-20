ba5e7b140ca2c28eb8f919c09b9eec13b446be77
Yuan <yuanhe777tt@hotmail.com>
2020-10-20 10:48:49 -0400

    qtl

--- QTL/QTL_calling.py
+++ QTL/QTL_calling.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import os
 import pdb
 import numpy as np
@@ -14,6 +15,7 @@ def read_in_peaks():
     samples = [x for x in peak_dat.columns if x.startswith('HG')]
     return [peak_dat, samples]
 
+
 def convert_gt_to_number(arri):
     '''
     convert genotype type A/A to 0, 1, 2 ...
@@ -22,7 +24,7 @@ def convert_gt_to_number(arri):
     nts = np.unique([a for b in [x.split('/') for x in arri[arri_idx]] for a in b])
     # remove the sites with more than two alleles
     if len(nts) > 2:
-        return 
+        return [-1] * len(arri)
     nts_dict = {}
     i = 0
     for ni in nts:
@@ -35,54 +37,49 @@ def convert_gt_to_number(arri):
 
 
 def obtain_numerical_gt(gt_dat, samples):
-    '''
-    Very slow
-    '''
 
     gt_numerical_dat = []
-    print('In total %d rows' % len(gt_dat))
-    for i in range(len(gt_dat)):
-        arri = np.array(gt_dat[samples].iloc[i])
-        token = convert_gt_to_number(arri)
-        if token is None:
-            continue
-        gt_numerical_dat.append(token)
-        if i % 500 == 0:
-            print('%d rows finished' % i)
-
+    print('In total %d variants are called' % len(gt_dat))
+    start = time.time()
+    gt_numerical_dat = map(convert_gt_to_number, np.array(gt_dat[samples]))
+    end = time.time()
+    print('Used %f s' % (end - start))
+    
     gt_numerical_dat = pd.DataFrame(gt_numerical_dat)
+
+    # remove rows with more than two alleles
+    validQTLsnps = np.where([len(set(x))>1 for x in np.array(gt_numerical_dat)])[0]
+    gt_numerical_dat = gt_numerical_dat.iloc[validQTLsnps].reset_index(drop=True)
+    gt_dat = gt_dat.iloc[validQTLsnps].reset_index(drop=True)
+ 
     gt_numerical_dat.columns = samples
     gt_numerical_dat['CHR_POS'] = gt_dat['CHR_POS']
     gt_numerical_dat['CHR'] = gt_dat['CHR']
     gt_numerical_dat['POS'] = gt_dat['POS']
     gt_numerical_dat = gt_numerical_dat[['CHR_POS', 'CHR', 'POS']  + samples]
     
-    gt_numerical_dat.to_csv('%s/gt_by_sample_matrix_chr%d_numericalGT.txt' % (Genotype_dir, CHROMOSOME), sep=' ', index = False)
-    
     return gt_numerical_dat
     
-
 
 
 def readin_genotype():
     ## Read in Genotpye
     gt_dat = pd.read_csv('%s/gt_by_sample_matrix_chr%d.txt' % (Genotype_dir, CHROMOSOME), sep=' ')
     gt_dat = gt_dat[[x for x in gt_dat.columns if 'Unnamed' not in x]]
-    samples = [x for x in gt_dat.columns if x.startswith('HG')]
+    
+    # match sample order in genotype matrix with the peak matrix
+    gt_dat = gt_dat[['CHR_POS', 'CHR', 'POS'] + SAMPLES]
 
     # remove rows with less than 3 samples
-    valid_snps = np.where(np.sum(np.array(gt_dat[samples]) != '0', axis=1) >= 3)[0]
+    valid_snps = np.where(np.sum(np.array(gt_dat[SAMPLES]) != '0', axis=1) >= 3)[0]
     gt_dat = gt_dat.iloc[valid_snps].reset_index(drop=True)
     
     # remove rows with only one genotype
-    validQTLsnps = np.where([len(set(x))>2 for x in np.array(gt_dat[samples])])[0]
+    validQTLsnps = np.where([len(set(x))>2 for x in np.array(gt_dat[SAMPLES])])[0]
     gt_dat = gt_dat.iloc[validQTLsnps].reset_index(drop=True)
 
-    try:
-        gt_numerical_dat = pd.read_csv('%s/gt_by_sample_matrix_chr%d_numericalGT.txt' % (Genotype_dir, CHROMOSOME), sep=' ')
-    except:
-        print('Convert genotype to numerical values')
-        gt_numerical_dat = obtain_numerical_gt(gt_dat, samples)
+    print('Convert genotype to numerical values')
+    gt_numerical_dat = obtain_numerical_gt(gt_dat, SAMPLES)
         
     return gt_numerical_dat
 
@@ -117,8 +114,6 @@ def derive_ll(info_dat, samples):
     post_pp_dat['POS'] = info_dat['POS']
     post_pp_dat = post_pp_dat[['CHR_POS', 'CHR', 'POS']  + samples]
     
-    post_pp_dat.to_csv('%s/gt_infoLL_by_sample_matrix_chr%d.txt' % (VCF_dir, CHROMOSOME), sep=' ', index = False)
-    
     return post_pp_dat
 
 
@@ -129,14 +124,15 @@ def readin_genotype_info(gt_dat):
     info_dat = pd.read_csv('%s/gt_info_by_sample_matrix_chr%d.txt' % (VCF_dir, CHROMOSOME), sep=' ')
     info_dat = info_dat[[x for x in info_dat.columns if 'Unnamed' not in x]]
     info_dat = info_dat.set_index('CHR_POS').loc[gt_dat['CHR_POS']].reset_index()
-    
-    samples = [x for x in info_dat.columns if x.startswith('HG')]
-    
-    try:
-        post_pp_dat = pd.read_csv('%s/gt_infoLL_by_sample_matrix_chr%d.txt' % (VCF_dir, CHROMOSOME), sep=' ')
-    except:
-        print('Derive posterior probability for the genotypes')
-        post_pp_dat = derive_ll(info_dat=info_dat, samples=samples)
+   
+    # match sample order in genotype matrix with the peak matrix
+    info_dat = info_dat[['CHR_POS', 'CHR', 'POS'] + SAMPLES]
+ 
+    print('Derive posterior probability for the genotypes')
+    start = time.time()
+    post_pp_dat = derive_ll(info_dat=info_dat, samples=SAMPLES)
+    end = time.time()
+    print('Used %f seconds' % (end - start))
         
     return post_pp_dat
 
@@ -148,56 +144,53 @@ def readin_genotype_info(gt_dat):
 # Genotype data weights: post_pp_dat
 
 
-def compute_QTL_gti_peaki(gt_sample, peak_sample, weight_sample):
+def compute_QTL_gti_peaki(datapoint):
     #gt_sample = np.array(gt_numerical_dat[samples].iloc[0])
     #peak_sample = np.array(RPKM_dat[samples].iloc[0])
     #weight_sample = np.array(post_pp_dat[samples].iloc[0])
+
+    [peak_sample, gt_sample, weight_sample] = datapoint
     valid_samples = np.where(gt_sample!= -1)[0]
     
     y = np.array(peak_sample[valid_samples])
     x = np.array(gt_sample[valid_samples])
     x_weights = np.array(weight_sample[valid_samples])
     
-    try:
-        wls_model = sm.WLS(y, x, weights = x_weights)
-        results = wls_model.fit()
-        return results.f_pvalue
-    except:
-        pdb.set_trace()
+    wls_model = sm.WLS(y, x, weights = x_weights)
+    results = wls_model.fit()
+
+    return results.f_pvalue
 
 
 
 def compute_QTL_peaki(peaki, gt_numerical_dat, post_pp_dat):
+
+    pdb.set_trace()
     QTL_result_peaki = []
-    #peaki = RPKM_dat[['START', 'END']].iloc[i]
     [start, end] = [peaki['START'], peaki['END']]
     
     SNPs_close = (np.array(gt_numerical_dat['POS']) < end + WINDOW) & (np.array(gt_numerical_dat['POS']) > start - WINDOW)
     SNPs_close = np.where(SNPs_close)[0]
     
-    SNPs_gt = gt_numerical_dat.iloc[SNPs_close]
-    SNPs_gt_ll = post_pp_dat.iloc[SNPs_close]
-    
-    for gti in range(len(SNPs_gt)):
-        print(peaki['PEAK'], SNPs_gt.iloc[gti]['CHR_POS'])
-        pval = compute_QTL_gti_peaki(SNPs_gt[samples].iloc[gti], peaki[samples], SNPs_gt_ll[samples].iloc[gti])
-        if(np.isnan(pval)):
-            break
-        snpid = SNPs_gt.iloc[gti]['CHR_POS']
-        peakid = peaki['PEAK']
-        QTL_result_peaki.append([peakid, snpid, pval])
-        
+    bb = np.array(gt_numerical_dat.iloc[SNPs_close][SAMPLES])
+    cc = np.array(post_pp_dat.iloc[SNPs_close][SAMPLES])
+    aa = np.repeat(np.array(peaki[SAMPLES])[np.newaxis], len(bb), axis=0)
+
+    datapoints = zip(aa,bb,cc)
+    pvalues = map(compute_QTL_gti_peaki, datapoints) 
+
+    QTL_result_peaki = pd.DataFrame({"PeakID": peaki['PEAK'], "CHR_POS": gt_numerical_dat.iloc[SNPs_close]['CHR_POS'], "P-value": pvalues})
     return QTL_result_peaki
 
 
 
 def compute_QTLs(WINDOW, peak_df, genotype_df, weight_df):
 
-    QTL_results = []
+    QTL_results = pd.DataFrame()
     for p in range(len(peak_df)):
         token = compute_QTL_peaki(peak_df.iloc[p], genotype_df, weight_df)
-        QTL_results.append([a for b in token for a in b])
-        if p % 1000 == 0:
+        QTL_results = QTL_results.append(token)
+        if (p+1) % 500 == 0:
             print('%d peaks finished' % p)
         
     QTL_results = [a for b in QTL_results for a in b]
@@ -208,7 +201,7 @@ def compute_QTLs(WINDOW, peak_df, genotype_df, weight_df):
    
     print('Tested %d associations for chromosome%d' % (len(QTL_dat), CHROMOSOME))
     print("") 
-    QTL_dat.to_csv('%s/CHR%d_acQTLs_WINDOW_%dkb.txt' % (QTL_dir, CHROMOSOME, WINDOW/1000), sep='\t', index = False)
+    #QTL_dat.to_csv('%s/CHR%d_acQTLs_WINDOW_%dkb.txt' % (QTL_dir, CHROMOSOME, WINDOW/1000), sep='\t', index = False)
 
 
 if __name__ == "__main__":
@@ -221,7 +214,7 @@ if __name__ == "__main__":
     VCF_dir = '/work-zfs/abattle4/heyuan/Variant_calling/datasets/GBR/ATAC_seq/alignment_bowtie/VCF_files'
     QTL_dir = '/work-zfs/abattle4/heyuan/Variant_calling/datasets/GBR/ATAC_seq/alignment_bowtie/QTLs'
 
-    [PEAK_DAT, samples] = read_in_peaks()
+    [PEAK_DAT, SAMPLES] = read_in_peaks()
     GT_DAT = readin_genotype()
     WEIGHT_DAT = readin_genotype_info(gt_dat=GT_DAT)
 
