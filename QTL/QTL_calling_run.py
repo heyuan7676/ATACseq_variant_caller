c0543098f450e5ed296084c545f635a18d5e26c3
Yuan <yuanhe777tt@hotmail.com>
2020-11-17 09:20:17 -0500

    format qtl calling

--- QTL/QTL_calling_run.py
+++ QTL/QTL_calling_run.py
@@ -7,8 +7,9 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression
 from statsmodels.regression import linear_model as sm
-from prepare_matrixQTL import *
+from prepare_data_matrix import *
 from QTL_calling import *
+from QTL_calling_WGS import *
 
 def read_in_peaks(PEAK_dir, chromosome):
     peak_dat = pd.read_csv('%s/peak_by_sample_matrix_RPKM_corrected_chromosome%d.txt' % (PEAK_dir, chromosome), sep='\t')
@@ -19,34 +20,59 @@ def read_in_peaks(PEAK_dir, chromosome):
 if __name__ == "__main__":
     CHROMOSOME = int(sys.argv[1])
     WINDOW = int(sys.argv[2])
-    SUFFIX = '_minDP2'
+    useWeight = sys.argv[3]
 
+    GT_subDir = 'minDP2'
+    SUFFIX = '_%s' % GT_subDir
     PEAK_dir = '/work-zfs/abattle4/heyuan/Variant_calling/datasets/GBR/ATAC_seq/alignment_bowtie/Peaks'
 
     # root_dir = '/work-zfs/abattle4/heyuan/Variant_calling/datasets/GBR/ATAC_seq/alignment_subsample_0.5'
     root_dir = '/work-zfs/abattle4/heyuan/Variant_calling/datasets/GBR/ATAC_seq/alignment_bowtie'
-    Genotype_dir = '%s/Called_GT/' % root_dir
+    Genotype_dir = '%s/Called_GT/%s' % (root_dir, GT_subDir)
     VCF_dir = '%s/VCF_files' % root_dir
-    QTL_dir = '%s/QTLs' % root_dir
-    #os.makedirs(QTL_dir, exist_ok = True)
+    QTL_dir = '%s/QTLs/%s' % (root_dir, GT_subDir)
+    os.makedirs(QTL_dir, exist_ok = True)
 
     print('Call ca-QTLs for chromosome %d with window = %dkb' % (CHROMOSOME, WINDOW/1000))
 
     ## read in data
-    [PEAK_DAT, SAMPLES] = read_in_peaks(PEAK_dir, CHROMOSOME)
+    [PEAK_DAT, SAMPLES_Peaks] = read_in_peaks(PEAK_dir, CHROMOSOME)
+
+    ## align the samples with samples from WGS
+    WGS_dir = '/work-zfs/abattle4/heyuan/Variant_calling/datasets/GBR/Genotype'
+    WGS_fn = '%s/%s.genotypes.tsv' % (WGS_dir, '1k_genome_chr22')
+    WGS_result = pd.read_csv(WGS_fn, comment = '$', sep='\t', nrows = 10)
+    SAMPLES_WGS = [x for x in WGS_result.columns if x.startswith('HG')]
+    
+    SAMPLES = list(np.intersect1d(SAMPLES_Peaks, SAMPLES_WGS))
+
+    print('%d samples are used for QTL analysis' % len(SAMPLES))
+
+    save_dir = os.path.join(QTL_dir)
+    try:
+        os.makedirs(save_dir)
+    except:
+        pass
 
     ## read in data
     GT_DAT = readin_genotype(Genotype_dir = Genotype_dir, chromosome=CHROMOSOME, samples=SAMPLES, suffix=SUFFIX)
-    #WEIGHT_DAT = readin_genotype_info(gt_dat=GT_DAT, VCF_dir = VCF_dir, chromosome=CHROMOSOME, samples=SAMPLES, suffix=SUFFIX)
-    ## compute QTLs
-    #compute_QTLs(CHROMOSOME, WINDOW, PEAK_DAT, GT_DAT, WEIGHT_DAT, QTL_dir, suffix=SUFFIX)
+    if useWeight:
+        WEIGHT_DAT = readin_genotype_info(gt_dat=GT_DAT, VCF_dir = VCF_dir, chromosome=CHROMOSOME, samples=SAMPLES, suffix=SUFFIX)
+        ## compute QTLs
+        compute_QTLs(CHROMOSOME, WINDOW, PEAK_DAT, GT_DAT, WEIGHT_DAT, QTL_dir, suffix=SUFFIX)
 
-    WEIGHT_DAT = GT_DAT.copy()
+    else:
+        WEIGHT_DAT = GT_DAT.copy()
+        WEIGHT_DAT[SAMPLES] = 1
+        ## compute QTLs
+        compute_QTLs(CHROMOSOME, WINDOW, PEAK_DAT, GT_DAT, WEIGHT_DAT, QTL_dir, suffix=SUFFIX, saveSuffix = '_noWeight')
+
+    ### Compute QTLs using genotype from WGS
+    [WGS_DAT, SAMPLES] = read_in_WGS_GT('1k_genome_chr%d' % CHROMOSOME, samples_peaks, WGS_dir, snps = np.array(GT_DAT['CHR_POS']))
+    WEIGHT_DAT = WGS_DAT.copy()
     WEIGHT_DAT[SAMPLES] = 1
 
-    ## compute QTLs
-    compute_QTLs(CHROMOSOME, WINDOW, PEAK_DAT, GT_DAT, WEIGHT_DAT, QTL_dir, suffix=SUFFIX, saveSuffix = '_noWeight')
-
+    compute_QTLs(CHROMOSOME, WINDOW, PEAK_DAT, WGS_DAT, WEIGHT_DAT, save_dir=save_dir, suffix=SUFFIX, saveSuffix='_realGT')
 
 
 
