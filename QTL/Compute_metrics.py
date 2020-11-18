9e3851791ee69425a3d8d667f0036a35533f5af4
Yuan <yuanhe777tt@hotmail.com>
2020-11-18 13:55:01 -0500

    evaluate QTLs

--- QTL/Compute_metrics.py
+++ QTL/Compute_metrics.py
@@ -7,76 +7,7 @@ import os
 from prepare_data_matrix import *
 from QTL_calling import *
 from statsmodels.stats.multitest import multipletests
-
-
-
-def readin_both_results(chromsome, window, suffix):
-    fn1 = '%s/CHR%d_caQTLs_WINDOW_%dkb%s.txt' % (QTL_dir, chromsome, window/1000, '_realGT')
-    real_QTL = pd.read_csv(fn1, sep='\t')
-    
-    fn2 = '%s/CHR%d_caQTLs_WINDOW_%dkb%s.txt' % (QTL_dir, chromsome, window/1000, suffix)
-    call_QTL = pd.read_csv(fn2, sep='\t')
-
-    real_QTL.columns = ['CHR_POS','pvalue_real', 'PeakID']
-    call_QTL.columns = ['CHR_POS','pvalue_called', 'PeakID']
-
-    QTLs_in_both = real_QTL.merge(call_QTL, on = ['CHR_POS', 'PeakID'])
-    
-    return QTLs_in_both
-
-
-
-
-def readin_both_results_matrixeQTL(chromsome, window, suffix):
-    fn1 = '%s/matrixeQTL_chromosome%d%s_cisDist%dkb%s.txt' % (QTL_dir, chromsome, suffix, window/1000, '_realGT')
-    real_QTL = pd.read_csv(fn1, sep='\t', usecols=[0, 1, 4])
-    
-    fn2 = '%s/matrixeQTL_chromosome%d%s_cisDist%dkb%s.txt' % (QTL_dir, chromsome, suffix, window/1000, '')
-    call_QTL = pd.read_csv(fn2, sep='\t', usecols=[0, 1, 4])
-
-    real_QTL.columns = ['CHR_POS', 'PeakID','pvalue_real']
-    call_QTL.columns = ['CHR_POS', 'PeakID','pvalue_called']
-
-    QTLs_in_both = real_QTL.merge(call_QTL, on = ['CHR_POS', 'PeakID'])
-    
-    return QTLs_in_both
-
-
-
-
-
-def readin_QTL_results(SAMPLEs, suffix):
-
-    QTLs_dat = pd.DataFrame()
-    QTLs_dat_PP = pd.DataFrame()
-
-    for CHROMOSOME in range(1,22):
-        try:
-            token = readin_both_results(CHROMOSOME, WINDOW, suffix)
-            QTLs_dat = QTLs_dat.append(token)
-        except:
-            print('chromosome%d not exist' % CHROMOSOME)
-            continue
-   
-        token_pp = readin_genotype_info(token, VCF_dir, CHROMOSOME, SAMPLEs)
-        QTLs_dat_PP = QTLs_dat_PP.append(token_pp[SAMPLEs])
-
-    QTLs_dat['min_nonzero_PP'] = [np.min(x[x>0]) for x in np.array(QTLs_dat_PP)]
-
-    QTLs_dat['PP_category'] = '0'
-    QTLs_dat.loc[QTLs_dat['min_nonzero_PP'] < 0.4, 'PP_category'] = '1'
-    QTLs_dat.loc[(QTLs_dat['min_nonzero_PP'] < 0.5) & (QTLs_dat['min_nonzero_PP'] >= 0.4), 'PP_category'] = '2'
-    QTLs_dat.loc[(QTLs_dat['min_nonzero_PP'] < 1) & (QTLs_dat['min_nonzero_PP'] >= 0.5), 'PP_category'] = '3'
-    QTLs_dat.loc[QTLs_dat['min_nonzero_PP'] == 1, 'PP_category'] = '4'
-
-    number_test_per_peak = pd.DataFrame(QTLs_dat.groupby('PeakID').size())
-    number_test_per_peak.columns = ['Number_tests']
-    QTLs_dat = QTLs_dat.merge(number_test_per_peak, left_on='PeakID', right_index=True)    
-    QTLs_dat['pvalue_real_Peak'] = list(map(lambda x: min(1, x), list(QTLs_dat['pvalue_real'] * QTLs_dat['Number_tests'])))
-    QTLs_dat['pvalue_called_Peak'] = list(map(lambda x: min(1, x), list(QTLs_dat['pvalue_called'] * QTLs_dat['Number_tests'])))
-
-    return QTLs_dat
-
+from Derive_QTLs import *
 
 
 def obtain_performance(QTLs_dat, alpha, save_file):
@@ -136,9 +67,6 @@ if __name__ == '__main__':
         PEAK_dir = '%s/Peaks_Genrich' % root_dir
         QTL_dir = '%s/QTLs_Genrich/%s' % (root_dir, GT_subDir)
 
-    if not os.path.exists(QTL_dir):
-        os.makedirs(QTL_dir)
-
     save_dir = 'Evaluation_metrics'
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
@@ -157,7 +85,7 @@ if __name__ == '__main__':
     
     alpha = 0.05
     for suffix in ['', '_noWeight']:
-        captured_QTLs = readin_QTL_results(SAMPLEs, suffix = '')
+        captured_QTLs = readin_QTL_results(QTL_dir, VCF_dir, SAMPLEs, WINDOW, suffix = '')
         obtain_performance(captured_QTLs, alpha, '%s/%s_%s%s.txt' % (save_dir, peak_calling, GT_subDir, suffix))
 
 
---
+++ QTL/Derive_QTLs.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+
+import sys
+import os
+
+from prepare_data_matrix import *
+from QTL_calling import *
+from statsmodels.stats.multitest import multipletests
+
+
+
+def readin_both_results(QTL_dir, chromsome, window, suffix):
+    fn1 = '%s/CHR%d_caQTLs_WINDOW_%dkb%s.txt' % (QTL_dir, chromsome, window/1000, '_realGT')
+    real_QTL = pd.read_csv(fn1, sep='\t')
+    
+    fn2 = '%s/CHR%d_caQTLs_WINDOW_%dkb%s.txt' % (QTL_dir, chromsome, window/1000, suffix)
+    call_QTL = pd.read_csv(fn2, sep='\t')
+
+    real_QTL.columns = ['CHR_POS','pvalue_real', 'PeakID']
+    call_QTL.columns = ['CHR_POS','pvalue_called', 'PeakID']
+
+    QTLs_in_both = real_QTL.merge(call_QTL, on = ['CHR_POS', 'PeakID'])
+    
+    return QTLs_in_both
+
+
+
+
+def readin_both_results_matrixeQTL(chromsome, window, suffix):
+    fn1 = '%s/matrixeQTL_chromosome%d%s_cisDist%dkb%s.txt' % (QTL_dir, chromsome, suffix, window/1000, '_realGT')
+    real_QTL = pd.read_csv(fn1, sep='\t', usecols=[0, 1, 4])
+    
+    fn2 = '%s/matrixeQTL_chromosome%d%s_cisDist%dkb%s.txt' % (QTL_dir, chromsome, suffix, window/1000, '')
+    call_QTL = pd.read_csv(fn2, sep='\t', usecols=[0, 1, 4])
+
+    real_QTL.columns = ['CHR_POS', 'PeakID','pvalue_real']
+    call_QTL.columns = ['CHR_POS', 'PeakID','pvalue_called']
+
+    QTLs_in_both = real_QTL.merge(call_QTL, on = ['CHR_POS', 'PeakID'])
+    
+    return QTLs_in_both
+
+
+
+
+
+def readin_QTL_results(QTL_dir, VCF_dir, SAMPLEs, window, suffix):
+
+    QTLs_dat = pd.DataFrame()
+    QTLs_dat_PP = pd.DataFrame()
+
+    for CHROMOSOME in range(1,22):
+        try:
+            token = readin_both_results(QTL_dir, CHROMOSOME, window, suffix)
+            QTLs_dat = QTLs_dat.append(token)
+        except:
+            print('chromosome%d not exist' % CHROMOSOME)
+            continue
+   
+        token_pp = readin_genotype_info(token, VCF_dir, CHROMOSOME, SAMPLEs)
+        QTLs_dat_PP = QTLs_dat_PP.append(token_pp[SAMPLEs])
+
+    QTLs_dat['min_nonzero_PP'] = [np.min(x[x>0]) for x in np.array(QTLs_dat_PP)]
+
+    QTLs_dat['PP_category'] = '0'
+    QTLs_dat.loc[QTLs_dat['min_nonzero_PP'] < 0.4, 'PP_category'] = '1'
+    QTLs_dat.loc[(QTLs_dat['min_nonzero_PP'] < 0.5) & (QTLs_dat['min_nonzero_PP'] >= 0.4), 'PP_category'] = '2'
+    QTLs_dat.loc[(QTLs_dat['min_nonzero_PP'] < 1) & (QTLs_dat['min_nonzero_PP'] >= 0.5), 'PP_category'] = '3'
+    QTLs_dat.loc[QTLs_dat['min_nonzero_PP'] == 1, 'PP_category'] = '4'
+
+    number_test_per_peak = pd.DataFrame(QTLs_dat.groupby('PeakID').size())
+    number_test_per_peak.columns = ['Number_tests']
+    QTLs_dat = QTLs_dat.merge(number_test_per_peak, left_on='PeakID', right_index=True)    
+    QTLs_dat['pvalue_real_Peak'] = list(map(lambda x: min(1, x), list(QTLs_dat['pvalue_real'] * QTLs_dat['Number_tests'])))
+    QTLs_dat['pvalue_called_Peak'] = list(map(lambda x: min(1, x), list(QTLs_dat['pvalue_called'] * QTLs_dat['Number_tests'])))
+
+    return QTLs_dat
+
+
+
+def Derive_QTLs(df):
+
+    group = df.copy()
+    
+    QTLs_real = group.loc[group.groupby('PeakID').pvalue_real_Peak.idxmin()]
+    QTLs_real['BH_pvalue_real'] = multipletests(QTLs_real['pvalue_real_Peak'], method='fdr_bh')[1]
+
+    QTLs_called = group.loc[group.groupby('PeakID').pvalue_called_Peak.idxmin()]
+    QTLs_called['BH_pvalue_called'] = multipletests(QTLs_called['pvalue_called_Peak'], method='fdr_bh')[1]
+
+    return [QTLs_real, QTLs_called]
+
+
