27151fae8a15ac7dd4b13041f31a1ec83fa3aecf
Yuan <yuanhe777tt@hotmail.com>
2020-12-07 18:22:03 -0500

    Compare_pvalues.py

--- GBR_benchmarking/QTL_calling/Compare_pvalues.py
+++ GBR_benchmarking/QTL_calling/Compare_pvalues.py
@@ -2,13 +2,17 @@ import numpy as np
 import pandas as pd
 from statsmodels.stats.multitest import multipletests
 import sys
-
+import pdb
 
 def readin_one_chromosome(QTL_dir, file_suffix):
     df = pd.DataFrame()
     for chromosome in range(1,2):
-	fn1 = '%s/CHR%_%s.txt' % (QTL_dir, file_suffix)
-    	dfi = pd.read_csv(fn1, sep='\t')
+	fn1 = '%s/CHR%d_%s' % (QTL_dir, chromosome, file_suffix)
+	try:
+	    dfi = pd.read_csv(fn1, sep='\t')
+	except:
+	    print('Result for chr%d not exist' % chromosome)
+	    continue
 	df = df.append(dfi)
     df_sig = df.loc[df.groupby('PeakID')["P-value"].idxmin()]
     df_sig['BH_pvalue'] = multipletests(df_sig['P-value'], method='fdr_bh')[1]
@@ -16,11 +20,11 @@ def readin_one_chromosome(QTL_dir, file_suffix):
     return [df[['PeakID', 'CHR_POS', 'P-value']], df_sig[['PeakID', 'CHR_POS', 'P-value', 'BH_pvalue']]]
 
 
-def readin_both_results(QTL_dir, chromsome, window, suffix):
+def readin_both_results(QTL_dir, window, suffix):
     
-    [real_QTL, real_QTL_sig] = readin_one_chromosome(QTL_dir, 'caQTLs_WINDOW_%skb%s.txt' % (str(window/1000.0), '_realGT_all')
-    [call_QTL, call_QTL_sig] = readin_one_chromosome(QTL_dir, 'caQTLs_WINDOW_%skb%s.txt' % (str(window/1000.0), suffix)
-    [call_QTL_imputed, call_QTL_imputed_sig] = readin_one_chromosome('%s/Imputation' % QTL_dir, 'caQTLs_WINDOW_%skb%s_withImputation%s.txt' % (str(window/1000.0), suffix)
+    [real_QTL, real_QTL_sig] = readin_one_chromosome(QTL_dir, 'caQTLs_WINDOW_%skb%s.txt' % (str(window/1000.0), '_realGT_all'))
+    [call_QTL, call_QTL_sig] = readin_one_chromosome(QTL_dir, 'caQTLs_WINDOW_%skb%s.txt' % (str(window/1000.0), suffix))
+    [call_QTL_imputed, call_QTL_imputed_sig] = readin_one_chromosome('%s/Imputation' % QTL_dir, 'caQTLs_WINDOW_%skb_withImputation%s.txt' % (str(window/1000.0), suffix))
 
     call_QTL.columns = ['PeakID', 'CHR_POS', 'Pvalue_genotype_caller']
     call_QTL_sig.columns = ['PeakID', 'CHR_POS', 'Pvalue_genotype_caller', 'BH_pvalue_genotype_caller']
@@ -61,24 +65,24 @@ def obtain_metrics(peak_calling, window, suffix = '_noWeight'):
 
     # genotype caller
     both_tested = df[(~df['Pvalue_genotype_caller'].isnull()) & (~df['P-value'].isnull())]
-    r2_caller = np.corrcoef(-np.log10(both_tested['P-value']), -np.log10(both_tested['Pvalue_genotype_caller']))
+    r2_caller = np.corrcoef(-np.log10(both_tested['P-value']), -np.log10(both_tested['Pvalue_genotype_caller']))[0, 1]
 
     # imputation
     both_tested = df[(~df['Pvalue_imputation'].isnull()) & (~df['P-value'].isnull())]
-    r2_imputation = np.corrcoef(-np.log10(both_tested['P-value']), -np.log10(both_tested['Pvalue_imputation']))
+    r2_imputation = np.corrcoef(-np.log10(both_tested['P-value']), -np.log10(both_tested['Pvalue_imputation']))[0, 1]
 
     print('Using genotype caller the Pearson correlation = %.2f; \n Using imputation the Pearson correlation = %.2f' % (r2_caller, r2_imputation))
 
-    set1 = set(df_sig[~df_sig['P-value'].isnull()].apply(lambda x: '_'.join((x['CHR_POS'], x['PeakID'])), axis=1))
-    set2 = set(df_sig[~df_sig['Pvalue_genotype_caller'].isnull()].apply(lambda x: '_'.join((x['CHR_POS'], x['PeakID'])), axis=1))
-    set3 = set(df_sig[~df_sig['Pvalue_imputation'].isnull()].apply(lambda x: '_'.join((x['CHR_POS'], x['PeakID'])), axis=1))
+    set1 = np.unique(df_sig[~df_sig['P-value'].isnull()].apply(lambda x: '_'.join((x['CHR_POS'], x['PeakID'])), axis=1))
+    set2 = np.unique(df_sig[~df_sig['Pvalue_genotype_caller'].isnull()].apply(lambda x: '_'.join((x['CHR_POS'], x['PeakID'])), axis=1))
+    set3 = np.unique(df_sig[~df_sig['Pvalue_imputation'].isnull()].apply(lambda x: '_'.join((x['CHR_POS'], x['PeakID'])), axis=1))
 
-    precision_caller = np.intersect1d(set1, set2) / len(set2)
-    recall_caller = np.intersect1d(set1, set2) / len(set1)
-    precision_imputation = np.intersect1d(set1, set3) / len(set3)
-    recall_imputation = np.intersect1d(set1, set3) / len(set1)
+    precision_caller = len(np.intersect1d(set1, set2)) / float(len(set2))
+    recall_caller = len(np.intersect1d(set1, set2)) / float(len(set1))
+    precision_imputation = len(np.intersect1d(set1, set3) )/ float(len(set3))
+    recall_imputation = len(np.intersect1d(set1, set3)) / float(len(set1))
 
-    result = [r2_caller, precision_caller, recall_caller, r2_imputation, precision_imputation, recall_imputation]
+    result = [peak_calling, window, r2_caller, r2_imputation, precision_caller, precision_imputation, recall_caller, recall_imputation]
 
     return result
 
@@ -86,5 +90,5 @@ def obtain_metrics(peak_calling, window, suffix = '_noWeight'):
 
 if __name__ == '__main__':
     for WINDOW in [0, 1000]:
-	pdb.set_trace()
     	result = obtain_metrics('macs2', WINDOW)
+	print(result)
