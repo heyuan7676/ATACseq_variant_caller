e9be6a3d0478c00f54bdc15f5bc44426d7e3f4c3
Yuan <yuanhe777tt@hotmail.com>
2021-04-28 11:43:08 -0400

    construct_VCF_Gencove

---
+++ GEO/QTL_calling/construct_VCF_Gencove.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import sys
+import gzip
+import pdb
+
+chromosome = int(sys.argv[1])
+VCF_dir = '/work-zfs/abattle4/heyuan/Variant_calling/datasets/GEO/Haematopoiesis/Gencove/merged_vcf_files'
+
+VCF_filename = '%s/chr%d.maf005.biallelic.recode.vcf.gz' % (VCF_dir, chromosome)
+
+header = []
+with gzip.open(VCF_filename,'rt') as f:
+    for line in f:
+        if line.rstrip().startswith('#'):
+            header.append(line.rstrip())
+        else:
+            break
+
+
+VCF_dat = pd.read_csv(VCF_filename, usecols=[0,1,2,3,4,5,6,7,8], comment='#', header=None, sep='\t')
+VCF_dat[2] = VCF_dat[[0,1]].apply(lambda x: '_'.join((str(x[0].replace('chr', '')), str(x[1]))), axis=1)
+
+dosage_file = '%s/chr%d.maf005.biallelic.recode.dosage.donors.txt' % (VCF_dir, chromosome)
+dosage_dat = pd.read_csv(dosage_file, sep='\t', index_col = [0])
+samples = [x for x in dosage_dat.columns if x.startswith('S')]
+
+merged_dat = VCF_dat.merge(dosage_dat[samples], left_on=2, right_index=True)
+merged_dat[8] = ['DS'] * len(merged_dat)
+#merged_dat = merged_dat[merged_dat[samples].apply(lambda x: np.sum(x) > 3, axis=1)]
+
+# modify the last line of header
+header[-1] = '\t'.join([x for x in header[-1].split('\t') if not x.startswith('S')] + list(samples))
+
+output_file = '%s/dosage_by_sample_matrix_chr%d.recode.forFastQTL.vcf' % (VCF_dir, chromosome)
+print(output_file)
+
+f = open(output_file, "w")
+for l in header:
+    f.write(l + '\n')
+
+
+for l in np.array(merged_dat):
+    f.write('\t'.join(map(str, l)) + '\n')
+
+f.close()
+
+
+
